fa317d45e7c79167e6fd66da3f1d3673321577c0
Md Jahid <mdjahid84206729@gmail.com>
2025-05-19 12:04:58 +0530

    main

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser,VendorProfile
 
 # Register your models here.
 @admin.register(CustomUser)
 class UserAdmin(admin.ModelAdmin):
-    list_display=['id','is_active','is_verified','name','email','phone','address','role']
+    list_display=['id','is_active','is_verified','name','email','password','phone','address','image']
+
+@admin.register(VendorProfile)
+class UserAdmin(admin.ModelAdmin):
+    list_display=['id','is_active','is_verified','name','email','password','shop_name','phone','gst_number','shop_logo','website']
--- account/models.py
+++ account/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 
-# Create your models here.
-USER_ROLES=(
-    
-    ('VENDOR','Vendor'),
-    ('CUSTOMER','Customer')
-)
 
 class Common(models.Model):
     is_active=models.BooleanField(default=True)
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now_add=True)
     is_verified=models.BooleanField(default=False)
+    
 
     class Meta:
         abstract=True
@@ -24,8 +19,25 @@ class CustomUser(Common):
     phone=models.CharField(max_length=10,null=True,blank=True)
     address=models.CharField(max_length=30,null=True,blank=True)
     image=models.ImageField(upload_to='image/',null=True,blank=True)
-    role=models.CharField(max_length=30,choices=USER_ROLES,default='CUSTOMER')
+    # role=models.CharField(max_length=30,choices=USER_ROLES,default='CUSTOMER')
       
     
     def __str__(self):
         return f'{self.name}'
+    
+    
+class VendorProfile(Common):
+    email=models.EmailField(unique=True)
+    name=models.CharField(max_length=30,null=True,blank=True)
+    password=models.CharField(max_length=30)
+    shop_name=models.CharField(max_length=100)
+    gst_number=models.IntegerField(null=True,blank=True)
+    phone=models.IntegerField(null=True,blank=True)
+    shop_address=models.CharField(max_length=30)
+    shop_logo=models.ImageField(upload_to='vendors/logo/',null=True,blank=True)
+    website=models.URLField(null=True,blank=True)
+    
+    
+    def __str__(self):
+        return f'{self.name}'
+    
